c2f87d4ecc3358f8e6895cc9b08a1395fddc962c
Rock718 <luo_xiao_hei_718@163.com>
2018-04-13 19:34:55 +0800

    M
    alpha

---
+++ Define.java
@@ -0,0 +1,6 @@
+public class Define {
+    public final static String PUBLIC_ID = "everyone";
+    public final static String PUBLIC_PASSWORD = "very-secret-password";
+    public final static String PUBLIC_PIN = "secret-pin";
+
+}
