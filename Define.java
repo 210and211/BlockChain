58c4187458d5bd21cf7c436fc21894fc47a29e7d
Rock718 <luo_xiao_hei_718@163.com>
2018-04-27 20:07:27 +0800

    Delete Define.java

--- Define.java
+++
@@ -1,6 +0,0 @@
-public class Define {
-    public final static String PUBLIC_ID = "everyone";
-    public final static String PUBLIC_PASSWORD = "very-secret-password";
-    public final static String PUBLIC_PIN = "secret-pin";
-
-}
