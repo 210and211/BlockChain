d224872bc506c52ccbcbbd17337bc8c99660a8a0
Rock718 <luo_xiao_hei_718@163.com>
2018-05-02 22:28:15 +0800

    Add files via upload

--- config/Configuration.java
+++ config/Configuration.java
@@ -1,1 +1,15 @@
+package config;
 
+public class Configuration {
+
+	public final static String PUBLIC_ID = "everyone";
+	public final static String PUBLIC_PASSWORD = "very-secret-password";
+	public final static String PUBLIC_PIN = "secret-pin";
+	
+	public final static String PUBLICKEY_PATH ="./publickey/";
+	public final static String PRIVATEKEY_PATH ="./privatekey/";
+	public final static int BYZANTINE_PEER_COUNT =3;
+	public final static int PORT =8000;
+	public final static String BLOCKCHAINPATH ="./block/";
+	public final static int BLOCK_DATA_MAX_NUM = 128;
+}
