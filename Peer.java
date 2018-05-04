329d054b99afc7d5058b4f25a19aa28b25c8afb7
AssassionXY <38351320+AssassionXY@users.noreply.github.com>
2018-05-04 17:29:34 +0800

    Add files via upload

--- Peer.java
+++ Peer.java
@@ -1,4 +1,4 @@
-package org.hive2hive.examples;
+package p2pPeer;
 
 import org.apache.commons.io.FileUtils;
 import org.hive2hive.core.api.H2HNode;
@@ -9,6 +9,8 @@ import org.hive2hive.core.api.interfaces.IH2HNode;
 import org.hive2hive.core.api.interfaces.IUserManager;
 import org.hive2hive.core.security.UserCredentials;
 import org.hive2hive.processframework.interfaces.IProcessComponent;
+
+import FileFragmentation_delete.FileFragmentation_delete;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -22,17 +24,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
-
+import FileFragmentation_delete.*;
 public class Peer {
 
     private IH2HNode node;
     private FileAgent fileAgent;
-    int port_Fragmentation_socket;//分片socket通信端口号
-    ArrayList<Integer> block_Fragmentationdelete_us;//哪个区块正在进行删除操作 这个需要在节点创造的同时定义一个相同的 然后给此变量赋节点变量的值
-    ArrayList<String> ip_list;//所有节点的ip地址
-    int blockchain_high;//区块总高度
-	int blockchain_range=500;//定义一个检查删除范围，比如现在区块高度为1000，我们只检查1000-500的区块删除，因为0-500的大概率已经删除到稳定节点个数
-	 
     public Peer(String directory){
         fileAgent = new FileAgent(new File(directory));
         node = H2HNode.createNode(FileConfiguration.createDefault());
@@ -92,14 +88,7 @@ public class Peer {
         return fileAgent;
     }
     
-    public void delete_run(){//循环一轮删除节点
-    	 for( int count=blockchain_high;count>(blockchain_high-blockchain_range);count--){
-			 block_Fragmentationdelete_us.set(count, 1);
-			 Thread DeletetThread=new Thread(new FileFragmentation_delete(count, port_Fragmentation_socket, ip_list, block_Fragmentationdelete_us));
-			 DeletetThread.start();
-			 block_Fragmentationdelete_us.set(count, 0);
-		 }
-    }
+   
 
     public static void main(String args[]){
         Peer peer1 = new Peer("C:\\Users\\new\\Desktop\\tmp1");
@@ -119,7 +108,7 @@ public class Peer {
             e.printStackTrace();
         }
         
-
+        Thread delete=new Thread(new FileFragmentation_delete_control(peer1));
     }
 
 }
