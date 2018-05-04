11e7b6acba94d9616e73cd59510368cb3c8363d9
AssassionXY <38351320+AssassionXY@users.noreply.github.com>
2018-05-04 18:24:43 +0800

    Add files via upload

--- config/Configuration.java
+++ config/Configuration.java
@@ -1,5 +1,7 @@
 package config;
 
+import java.util.ArrayList;
+
 public class Configuration {
 
 	public final static String PUBLIC_ID = "everyone";
@@ -12,4 +14,11 @@ public class Configuration {
 	public final static int PORT =8000;
 	public final static String BLOCKCHAINPATH ="./block/";
 	public final static int BLOCK_DATA_MAX_NUM = 128;
+	public final static int least_exit=10;
+	public final static int exit_all_time=1000;
+	public final static int exit_only_time=1000;
+	public final static ArrayList<Integer> block_Fragmentationdelete_us=new ArrayList<Integer>();//哪个区块正在进行删除操作 这个需要在节点创造的同时定义一个相同的 然后给此变量赋节点变量的值
+	public final static int port_Fragmentation_socket=8000;//分片socket通信端口号
+	public final static ArrayList<String> ip_list=new ArrayList<String>();//所有节点的ip地址
+	public final static int blockchain_high=1;//区块总高度
 }
