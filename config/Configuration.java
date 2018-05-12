ac4a0bf994ca96c83def59ffd8ed8a756fb66c8d
AssassionXY <38351320+AssassionXY@users.noreply.github.com>
2018-05-12 10:47:59 +0800

    Add files via upload

--- config/Configuration.java
+++ config/Configuration.java
@@ -1,5 +1,6 @@
 package config;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Configuration {
@@ -17,8 +18,9 @@ public class Configuration {
 	public final static int least_exit=10;
 	public final static int exit_all_time=1000;
 	public final static int exit_only_time=1000;
-	public final static ArrayList<Integer> block_Fragmentationdelete_us=new ArrayList<Integer>();//哪个区块正在进行删除操作 这个需要在节点创造的同时定义一个相同的 然后给此变量赋节点变量的值
+	public static ArrayList<Integer> block_Fragmentationdelete_us=new ArrayList<Integer>();//哪个区块正在进行删除操作 这个需要在节点创造的同时定义一个相同的 然后给此变量赋节点变量的值
 	public final static int port_Fragmentation_socket=8000;//分片socket通信端口号
-	public final static ArrayList<String> ip_list=new ArrayList<String>();//所有节点的ip地址
-	public final static int blockchain_high=1;//区块总高度
+	public static ArrayList<String> ip_list=new ArrayList<String>();//所有节点的ip地址
+	public static int blockchain_high;//区块总高度
+	public static ArrayList<LocalDate> block_saccessTime=new ArrayList<LocalDate>();//存在区块的最新访问时间
 }
