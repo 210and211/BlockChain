602d5436b1eea7bbde3444c9049dccf691a8a0fd
a1061086424 <31586087+a1061086424@users.noreply.github.com>
2018-05-12 11:05:01 +0800

    Add files via upload

--- consensus/Server.java
+++ consensus/Server.java
@@ -1,7 +1,6 @@
 package consensus;
 
 import java.io.File;
-import config.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -13,38 +12,50 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashSet;
-import p2pPeer.*;
 
-public class Server extends Thread{
+import block.Block;
+import block.MedicalRecords;
+import config.Configuration;
+import net.tomp2p.p2p.Peer;
+
+public class Server extends Thread {
 	HashSet<ArrayList<String>> set;
 	ArrayList<Byzantine_socket_info> bsi;
 	int[] cicle;
+	HashSet<MedicalRecords> block_list = new HashSet<MedicalRecords>();
+	HashSet<String> hash_list = null;
+	Block block=null;
 	@Override
-	public void run(){
+	public void run() {
 		ServerSocket serverSocket;
 		try {
-			serverSocket = new ServerSocket(Configuration.port);
-			while(true){
-				Socket socket=serverSocket.accept();
-				ServerSocketThread sht=new ServerSocketThread(socket);
+			serverSocket = new ServerSocket(Configuration.PORT);
+			while (true) {
+				Socket socket = serverSocket.accept();
+				ServerSocketThread sht = new ServerSocketThread(socket);
 				sht.start();
 				InetAddress inetAddress = socket.getInetAddress();
-				System.out.println("当前客户端的IP地址是："+inetAddress.getHostAddress());
+				System.out.println("当前客户端的IP地址是：" + inetAddress.getHostAddress());
 			}
 		} catch (IOException e) {
 			// TODO 自动生成的 catch 块
 			e.printStackTrace();
 		}
-		
+
 	}
-	void set_byzantine(HashSet<ArrayList<String>> set,
-			ArrayList<Byzantine_socket_info> bsi,int[] cicle) {
-		this.set=set;
-		this.bsi=bsi;
-		this.cicle=cicle;
+
+	void set_byzantine_hash(HashSet<ArrayList<String>> set, ArrayList<Byzantine_socket_info> bsi, int[] cicle) {
+		this.set = set;
+		this.bsi = bsi;
+		this.cicle = cicle;
 	}
-	class ServerSocketThread extends Thread{
-	    Socket socket=null;
+
+	void set_byzantine_block(HashSet<String> hash_list) {
+		this.hash_list = hash_list;
+	}
+
+	class ServerSocketThread extends Thread {
+		Socket socket=null;
 	    Peer peer;//添加一个peer变量  传入参数为当前结点
 	    ServerSocketThread(Socket socket) {
 	        this.socket = socket;
@@ -52,31 +63,52 @@ public class Server extends Thread{
 	    public void peer_value(Peer peer){
 	    	this.peer=peer;
 	    }
-	    @Override
-	    public void run() {
-	        // TODO Auto-generated method stub
-	    	ArrayList<Integer> block_Fragmentationdelete_us;//这个值必须与peer里面的绑定 因为这个变量值时刻在变化
-	        OutputStream os = null;
-	        PrintWriter pw = null;
-	        try {
-	            InputStream is = socket.getInputStream();
-	            ObjectInputStream ois=new ObjectInputStream(is);
-	            //readObject()方法必须保证服务端和客户端的User包名一致，要不然会出现找不到类的错误
-	            Byte mark1=ois.readByte();
-	            
-	            if(mark1==2) {
-	            	Byte mark2=ois.readByte();
-	            	cicle[mark2]++;
-	            	System.out.println(cicle[mark2]+" "+mark2);
-	            	Byzantine_socket_info[] info=(Byzantine_socket_info[]) ois.readObject();
-	                for (int i = 0; i < info.length; i++) {
-	                	if(info[i].verifySign()) {
-	    	            	if(set.add(info[i].get_info()))
-	    	            		bsi.add(info[i]);          		
-	    	            	
-	    	            }	
-	    			}
-	            }else if(mark1==3) {
+
+		
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			OutputStream os = null;
+			PrintWriter pw = null;
+			try {
+				InputStream is = socket.getInputStream();
+				ObjectInputStream ois = new ObjectInputStream(is);
+				// readObject()方法必须保证服务端和客户端的User包名一致，要不然会出现找不到类的错误
+				Byte mark1 = ois.readByte();
+				
+				if (mark1 == 1) {
+					MedicalRecords medicalRecords = (MedicalRecords) ois.readObject();
+					//发给接收方
+					
+					
+				} else if (mark1 == 2) {
+					Byte mark2 = ois.readByte();
+					if (mark2 == -1) {
+						MedicalRecords[] list = (MedicalRecords[]) ois.readObject();
+						while (hash_list == null) {
+							Thread.sleep(100);
+						}
+						for (int i = 0; i < list.length; i++) {
+							if (hash_list.contains(MD5Util.md5(list[i].toByteArray()))) {
+								block_list.add(list[i]);
+							}
+						}
+					} else if(mark2 == -2) {
+						block=(Block) ois.readObject();
+					}else if (mark2 < cicle.length) {
+						cicle[mark2]++;
+						System.out.println(cicle[mark2] + " " + mark2);
+						Byzantine_socket_info[] info = (Byzantine_socket_info[]) ois.readObject();
+						for (int i = 0; i < info.length; i++) {
+							if (info[i].verifySign()) {
+								if (set.add(info[i].get_info())) {
+									bsi.add(info[i]);
+								}
+							}
+						}
+					}
+				} else if(mark1==3) {
 	            	String s=(String) ois.readObject();
 	            	int count=Integer.valueOf(s).intValue();
 	            	int exit_blockchain = 0;//区块是否存在 缺省为不存在
@@ -106,29 +138,29 @@ public class Server extends Thread{
 		            oos.close();
 		            os.close();
 	            }
-	            
-	        } catch (Exception e) {
-	            // TODO Auto-generated catch block
-	            e.printStackTrace();
-	        }finally{
-	            try{
-	                if(pw!=null){
-	                    pw.close();
-	                }
-	                if(os!=null){
-	                    os.close();
-	                }
-	                if(socket!=null){
-	                    socket.close();
-	                }
-	            }catch (IOException e) {
-	                    // TODO Auto-generated catch block
-	                    e.printStackTrace();
-	            }
 
-	        }
-	    }
-	    
-	} 
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				try {
+					if (pw != null) {
+						pw.close();
+					}
+					if (os != null) {
+						os.close();
+					}
+					if (socket != null) {
+						socket.close();
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		}
+
+	}
 
 }
