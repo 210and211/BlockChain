02e2fd8f1da96e97ea4b90725785cbf28f5886f8
a1061086424 <31586087+a1061086424@users.noreply.github.com>
2018-05-13 20:44:51 +0800

    Add files via upload

--- config/Configuration.java
+++ config/Configuration.java
@@ -21,7 +21,12 @@ public class Configuration {
 	public final static long create_block_time=1*60*1000;
 	public static ArrayList<Integer> block_Fragmentationdelete_us=new ArrayList<Integer>();//哪个区块正在进行删除操作 这个需要在节点创造的同时定义一个相同的 然后给此变量赋节点变量的值
 	public final static int port_Fragmentation_socket=8000;//分片socket通信端口号
-	public static ArrayList<String> ip_list=new ArrayList<String>();//所有节点的ip地址
+	public final static String[] ip_list= {"192.168.1.107","192.168.1.106","192.168.1.100"};
 	public static int blockchain_high;//区块总高度
 	public static ArrayList<LocalDate> block_saccessTime=new ArrayList<LocalDate>();//存在区块的最新访问时间
+	
 }
+	
+	
+	
+	
