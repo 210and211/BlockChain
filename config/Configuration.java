8d57ec90eea98756765178a4543baf957cdc9660
a1061086424 <31586087+a1061086424@users.noreply.github.com>
2018-05-13 19:58:47 +0800

    Update Configuration.java

--- config/Configuration.java
+++ config/Configuration.java
@@ -18,6 +18,7 @@ public class Configuration {
 	public final static int least_exit=2;
 	public final static int exit_all_time=10000;
 	public final static int exit_only_time=120000;
+	public final static long create_block_time=1*60*1000;
 	public static ArrayList<Integer> block_Fragmentationdelete_us=new ArrayList<Integer>();//哪个区块正在进行删除操作 这个需要在节点创造的同时定义一个相同的 然后给此变量赋节点变量的值
 	public final static int port_Fragmentation_socket=8000;//分片socket通信端口号
 	public static ArrayList<String> ip_list=new ArrayList<String>();//所有节点的ip地址
