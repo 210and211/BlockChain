472da1275439123ba794bc8b057eb3e6d3ff5163
AssassionXY <38351320+AssassionXY@users.noreply.github.com>
2018-05-17 09:42:12 +0800

    Add files via upload

--- config/Configuration.java
+++ config/Configuration.java
@@ -15,18 +15,15 @@ public class Configuration {
 	public final static int PORT =8000;
 	public final static String BLOCKCHAINPATH ="./block/";
 	public final static int BLOCK_DATA_MAX_NUM = 128;
-	public final static int least_exit=2;
+	public final static int least_exit=1;
 	public final static int exit_all_time=10000;
-	public final static int exit_only_time=120000;
+	public final static int exit_only_time=1000*3600*48;
 	public final static long create_block_time=1*60*1000;
-	public static ArrayList<Integer> block_Fragmentationdelete_us=new ArrayList<Integer>();//哪个区块正在进行删除操作 这个需要在节点创造的同时定义一个相同的 然后给此变量赋节点变量的值
 	public final static int port_Fragmentation_socket=8000;//分片socket通信端口号
-	public final static String[] ip_list= {"192.168.1.107","192.168.1.106","192.168.1.100"};
-	public static int blockchain_high;//区块总高度
+	public final static String[] ip_list= {"192.168.1.104","192.168.1.107","192.168.1.106"};
+	public static int blockchain_high=21;//区块总高度
 	public static ArrayList<LocalDate> block_saccessTime=new ArrayList<LocalDate>();//存在区块的最新访问时间
+	public static int blockchain_range=500;//定义一个检查删除范围，比如现在区块高度为1000，我们只检查1000-500的区块删除，因为0-500的大概率已经删除到稳定节点个数
+	
 	
 }
-	
-	
-	
-	
