f1a310ecee89169e3b26dd1f6a5fbe415e63b2a5
AssassionXY <38351320+AssassionXY@users.noreply.github.com>
2018-05-25 14:15:43 +0800

    Add files via upload

--- block/MedicalRecords.java
+++ block/MedicalRecords.java
@@ -3,17 +3,18 @@ package block;
 
 import consensus.MD5Util;
 
-import javax.swing.text.Document;
+import org.jdom.Document;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDate;
+
 
 public class MedicalRecords implements Serializable {
     private long preBlockIndex;         //患者前一条记录的区块高度（默认为0，即无前一区块）
     private boolean createOrObtain;     //创建病历或查阅病历，创建true为查阅为false
-    private Date operateTime;           //信息产生时间
+    private LocalDate operateTime;           //信息产生时间
     private int opHospitalID;       //产生此操作记录的医院ID，若为创建操作则与hospitalID相同，否则为发起查询医院的ID
     private int infoID;             //通过patientID和infoID唯一确定患者的病历，每位患者的病历从1开始计数
     /*病历信息*/
@@ -24,7 +25,7 @@ public class MedicalRecords implements Serializable {
     private Document info = null;       //病历内容(若为获取病历信息则此条为null)
 
     //构建一条创建病例的记录（如无前一条病例所在区块，则preBlockIndex=0）
-    public MedicalRecords(long preBlockIndex, Date operateTime, int infoID, int hospitalID, long patientID, String section, String sign, Document info){
+    public MedicalRecords(long preBlockIndex, LocalDate operateTime, int infoID, int hospitalID, long patientID, String section, String sign, Document info){
         this.preBlockIndex = preBlockIndex;
         this.createOrObtain = true;
         this.operateTime = operateTime;
@@ -38,7 +39,7 @@ public class MedicalRecords implements Serializable {
     }
 
     //构建一条查阅病例的记录（如无前一条病例所在区块，则preBlockIndex=0）
-    public MedicalRecords(long preBlockIndex, Date operateTime,int opHospitalID, int infoID, int hospitalID, long patientID, String section){
+    public MedicalRecords(long preBlockIndex, LocalDate operateTime,int opHospitalID, int infoID, int hospitalID, long patientID, String section){
         this.preBlockIndex = preBlockIndex;
         this.createOrObtain = false;
         this.operateTime = operateTime;
@@ -55,7 +56,7 @@ public class MedicalRecords implements Serializable {
     public boolean getCreateOrObtion(){
         return createOrObtain;
     }
-    public Date getOperateTime(){
+    public LocalDate getOperateTime(){
         return operateTime;
     }
 
